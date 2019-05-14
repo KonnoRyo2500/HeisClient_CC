b07dab3f85b75c58ab13a195e3791ea5ce1dc7f0
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2019-05-14 23:58:07 +0900

    ・司令官クラスに関数宣言を追加 ・一部関数をconst化(実装時に外れる可能性あり)

--- HeisAPI/src/commander.h
+++ HeisAPI/src/commander.h
@@ -15,10 +15,16 @@ class CCommander
 		// コンストラクタ
 		explicit CCommander(const std::string& team_name);
 
+		// ステータスの取得
+		uint16_t get_x_position(const std::string& id) const;
+		uint16_t get_y_position(const std::string& id) const;
+		uint8_t get_action_remain(const std::string& id) const;
+		uint8_t get_hp(const std::string& id) const;
+
 		// 行動(指定したIDを持つ兵士への命令)
-		void attack(const std::string& id, const Direction direction);
-		void move(const std::string& id, const Direction direction);
-		void double_move(const std::string& id, const Direction first_move_direction, const Direction second_move_direction);
+		void attack(const std::string& id, const Direction direction) const;
+		void move(const std::string& id, const Direction direction) const;
+		void double_move(const std::string& id, const Direction first_move_direction, const Direction second_move_direction) const;
 
 		// 行動可能な兵士の取得
 		std::vector<std::string&> get_all_movable_infantry_ids();
@@ -27,10 +33,10 @@ class CCommander
 		void update_all_infantries();
 
 	private:
-		CInfantry* find_infantry_by_id(const std::string& id);
+		CInfantry* find_infantry_by_id(const std::string& id) const;
 
 	// メンバ変数
 	private:
-		std::string m_team_name;
+		std::string m_team_name;					// チーム名
 		std::vector<CInfantry*> m_infantries;		// チームの全兵士
 };
