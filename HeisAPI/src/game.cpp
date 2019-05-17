817f570bf865e73f4ba6cf13ca81d03fe803e346
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2019-05-17 21:02:57 +0900

    ・ゲーム進行管理クラスCGameクラスを新規作成

---
+++ HeisAPI/src/game.cpp
@@ -0,0 +1,4 @@
+﻿// heis ゲーム進行管理クラス
+// Author: Ryo Konno
+
+#include "game.h"
---
+++ HeisAPI/src/game.h
@@ -0,0 +1,14 @@
+﻿// heis ゲーム進行管理クラス
+// Author: Ryo Konno
+#pragma once
+
+class CGame {
+	// 構造体，列挙体など
+
+	// メンバ関数
+	public:
+		
+	private:
+
+	// メンバ変数
+};
