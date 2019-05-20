0d9cdc54de5bf780b6ca20ef41ccef526d9e3f0c
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2019-05-20 22:23:06 +0900

    ・サンプルのAIを作成する(行動や移動方向をランダムに選択する単純なAI) ・関数を実装中

--- HeisAPI/src/AI.cpp
+++ HeisAPI/src/AI.cpp
@@ -2,6 +2,7 @@
 // Author: Ryo Konno
 
 #include "AI.h"
+#include <random>
 
 /* public関数 */
 
@@ -25,3 +26,44 @@ void CUserAI::AI_main()
 }
 
 /* private関数 */
+
+/*
+	(サンプルAI用処理)次に行動する兵士をランダムに選択する関数
+	引数1: const std::vector<CInfantry*>& infantries 行動可能なすべての兵士
+	返り値: CInfantry* 次に行動する兵士
+*/
+CInfantry* CUserAI::sample_select_next_infantry(const std::vector<CInfantry*>& infantries) const
+{
+	std::random_device rnd_dev;
+	int infantry_idx = rnd_dev() % infantries.size();
+
+	return infantries[infantry_idx];
+}
+
+/*
+	(サンプルAI用処理)兵士に対して命令する行動を決定する関数
+	引数なし
+	返り値: CUserAI::SampleAction 次の行動
+*/
+CUserAI::SampleAction CUserAI::sample_decide_action() const
+{
+	std::random_device rnd_dev;
+	int action_kind = rnd_dev() % 2;
+
+	if (action_kind == 0) {
+		return SampleAction_Move;
+	}
+	else {
+		return SampleAction_Attack;
+	}
+}
+
+/*
+	(サンプルAI用処理)行動の方向を決定する関数
+	引数なし
+	返り値: Direction 方向
+*/
+Direction CUserAI::sample_decide_direction() const
+{
+	return Direction_Right;
+}
--- HeisAPI/src/AI.h
+++ HeisAPI/src/AI.h
@@ -7,6 +7,11 @@
 // 対戦用のAI(ユーザAI)は，このクラスに記述すること(必要があれば，メンバ関数やメンバ変数，クラスを新たに追加してもよい)
 class CUserAI {
 	// 構造体，列挙体など
+	private:
+		enum SampleAction {
+			SampleAction_Move,
+			SampleAction_Attack,
+		};
 
 	// メンバ関数
 	public:
@@ -15,6 +20,12 @@ class CUserAI {
 
 		// メイン処理
 		void AI_main();
+
+	private:
+		// サンプルAI用の処理
+		CInfantry* sample_select_next_infantry(const std::vector<CInfantry*>& infantries) const;
+		SampleAction sample_decide_action() const;
+		Direction sample_decide_direction() const;
 
 	// メンバ変数
 	private:
