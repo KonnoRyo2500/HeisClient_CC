66372895119d105f713124f66d146a96b89edd31
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2019-05-26 22:51:10 +0900

    ・ソケット通信用のクラスCSocketを新規追加

---
+++ HeisAPI/src/socket.cpp
@@ -0,0 +1,2 @@
+﻿// heis TCP/IP通信用ソケットクラス
+// Author: Ryo Konno
---
+++ HeisAPI/src/socket.h
@@ -0,0 +1,14 @@
+﻿// heis TCP/IP通信用ソケットクラス
+// Author: Ryo Konno
+#pragma once
+class CSocket {
+	// 構造体，列挙体など
+
+	// メンバ関数
+	public:
+
+	private:
+
+	// メンバ変数
+	private:
+};
