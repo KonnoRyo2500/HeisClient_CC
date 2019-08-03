0ecaaed5d833ec5234e7d3b74acec0fc72dd117b
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2019-08-03 21:01:52 +0900

    ・プログラムで共通して使う構造体やクラスを宣言するヘッダファイルを作成 ・ただし，今後定数と合わせて別のヘッダファイルにまとめる可能性がある

---
+++ HeisClient_CC/src/common_structure.h
@@ -0,0 +1,14 @@
+﻿// heis 共通データ構造
+// Author: Ryo Konno
+#pragma once
+
+#include <cstdint>
+
+// プログラム中で共通して使う構造体やクラスをここに記述する
+
+// フィールド上の座標
+struct FieldCoodinate 
+{	
+	uint16_t x;
+	uint16_t y;
+};
