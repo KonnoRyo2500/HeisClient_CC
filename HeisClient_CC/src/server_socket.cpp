c419667b2ebbbbf28854310ad29a74392fe6642e
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2019-11-06 00:15:09 +0900

    ・サーバ用のTCPソケットクラスCServerSocketクラスを新規追加

---
+++ HeisClient_CC/src/server_socket.cpp
@@ -0,0 +1,4 @@
+﻿// heis TCP/IP通信用ソケットクラス(サーバ用)
+// Author: Ryo Konno
+
+#include "server_socket.h"
---
+++ HeisClient_CC/src/server_socket.h
@@ -0,0 +1,8 @@
+﻿// heis TCP/IP通信用ソケットクラス(サーバ用)
+// Author: Ryo Konno
+#pragma once
+
+// TODO: このクラスを共通ソースに移動させる
+class CServerSocket {
+
+};
