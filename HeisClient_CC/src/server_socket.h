7b198c417a00f8b62d6d2dc27ce9d1e62f545bc8
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2019-11-06 22:32:45 +0900

    ・関数のプロトタイプ宣言を一通り記載 ・CClientSocketクラスを参考にした

--- HeisClient_CC/src/server_socket.h
+++ HeisClient_CC/src/server_socket.h
@@ -2,7 +2,44 @@
 // Author: Ryo Konno
 #pragma once
 
+#include <string>
+#include <vector>
+
 // TODO: このクラスを共通ソースに移動させる
 class CServerSocket {
+	// 構造体，列挙体など
+	private:
+		// ソケットに関する諸定数
+		enum SocketConstVal {
+			SocketConstVal_RecvBufSize = 10,		// 受信バッファサイズ
+		};
 
+	// メンバ関数
+	public:
+		// コンストラクタ
+		explicit CServerSocket();
+
+		// デストラクタ
+		~CServerSocket();
+
+		// クライアントからの接続待ち
+		void sck_accept();
+		void sck_listen();
+
+		// 送受信
+		void sck_send(const std::string& msg) const;
+		std::string sck_recv() const;
+
+	private:
+		// 初期化，終了処理
+		void initialize_socket() const;
+		void sck_socket();
+		void finalize_socket() const;
+
+	// メンバ変数
+	private:
+		// ソケットの実体(接続受け付け用)
+		int m_sck_accept;
+		// ソケットの実体(クライアントとの通信用)
+		std::vector<int> m_sck_com;
 };
