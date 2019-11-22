11bc6a8a7f0b705b03d36e46376e49d077b39378
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2019-11-22 23:01:27 +0900

    ・CSV設定ファイル読み込みクラスCCsvSettingFileReaderを追加

---
+++ HeisClient_CC/src/csv_setting_file_reader.cpp
@@ -0,0 +1,8 @@
+﻿// CSV設定ファイルクラス
+// Author: Ryo Konno
+
+#include "csv_setting_file_reader.h"
+
+/* public関数 */
+
+/* private関数 */
---
+++ HeisClient_CC/src/csv_setting_file_reader.h
@@ -0,0 +1,15 @@
+﻿// CSV設定ファイルクラス
+// Author: Ryo Konno
+#pragma once
+
+// TODO: このクラスを共通ソースに移動させる
+class CCsvSettingFileReader {
+	// 構造体，列挙体など
+	public:
+
+	// メンバ関数
+	public:
+
+	// メンバ変数
+	private:
+};
