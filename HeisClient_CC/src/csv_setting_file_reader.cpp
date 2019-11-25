e33fc79881e5d526153f25ad9228a6972bc428a1
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2019-11-25 22:55:16 +0900

    ・CSV設定ファイルクラスを実装完了

--- HeisClient_CC/src/csv_setting_file_reader.cpp
+++ HeisClient_CC/src/csv_setting_file_reader.cpp
@@ -4,5 +4,55 @@
 #include "csv_setting_file_reader.h"
 
 /* public関数 */
+/*
+	コンストラクタ
+	引数1: const std::string& file_name 設定ファイル名
+	例外: ファイルオープンに失敗したとき
+*/
+CCsvSettingFileReader::CCsvSettingFileReader(const std::string& file_name)
+	:m_file(file_name)
+{
+	if (m_file.fail()) {
+		throw CHeisClientException("設定ファイルのオープンに失敗しました(ファイル名: %s)", file_name.c_str());
+	}
+}
+
+/*
+	デストラクタ
+	引数なし
+*/
+CCsvSettingFileReader::~CCsvSettingFileReader()
+{
+	// 処理なし
+}
 
 /* private関数 */
+/*
+	指定されたキーの値をCSV設定ファイルからすべて読み込む関数
+	引数1: const std::string& key キー名
+	返り値: token_array_t 値を表すトークンの集合
+	例外: 指定したキーがファイルになかったとき
+*/
+token_array_t CCsvSettingFileReader::read_all_value(const std::string& key)
+{
+	std::string str;
+	token_array_t key_value;
+	CTokenManager tm;
+
+	// キーを検索
+	while (std::getline(m_file, str)) {
+		key_value = tm.split_string(str, ",");
+		if (key_value.size() > 0 && key_value[0] == key) {
+			key_value.erase(key_value.begin());
+			// 次回この関数を呼び出したときにファイルを先頭から読み込めるよう，読み出し位置を戻す
+			m_file.seekg(0, std::ios_base::beg);
+			// キー(先頭要素)を削除しているので，実際に返しているのは値のみ
+			return key_value;
+		}
+	}
+
+	// キー検索失敗
+	// 次回この関数を呼び出したときにファイルを先頭から読み込めるよう，読み出し位置を戻す
+	m_file.seekg(0, std::ios_base::beg);
+	throw CHeisClientException("キーが見つかりませんでした(キー名: %s)", key.c_str());
+}
--- HeisClient_CC/src/csv_setting_file_reader.h
+++ HeisClient_CC/src/csv_setting_file_reader.h
@@ -2,14 +2,172 @@
 // Author: Ryo Konno
 #pragma once
 
+#include <fstream>
+#include <vector>
+
+#include "token_manager.h"
+#include "heis_client_exception.h"
+
 // TODO: このクラスを共通ソースに移動させる
 class CCsvSettingFileReader {
 	// 構造体，列挙体など
 	public:
-
+		
 	// メンバ関数
 	public:
+		// コンストラクタ
+		explicit CCsvSettingFileReader(const std::string& file_name);
+
+		// デストラクタ
+		~CCsvSettingFileReader();
+
+		// 値の取得
+		// TODO: 処理を見直す(よりきれいな処理にできないか?)
+		template <typename T>
+		T get_single_value(const std::string& key, const size_t index);
+		template <typename T>
+		std::vector<T> get_all_value(const std::string& key);
+		template <typename T>
+		std::vector<T> get_ranged_value(const std::string& key, const size_t begin_index, const size_t end_index);
+
+		// 値の取得(特定の型に対する処理)
+		template<>
+		std::string get_single_value<std::string>(const std::string& key, const size_t index);
+		template<>
+		std::vector<std::string> get_all_value<std::string>(const std::string& key);
+		template<>
+		std::vector<std::string> get_ranged_value<std::string>(const std::string& key, const size_t begin_index, const size_t end_index);
+
+	private:
+		// 値の読み込み
+		token_array_t read_all_value(const std::string& key);
 
 	// メンバ変数
 	private:
+		// CSV設定ファイル
+		std::ifstream m_file;
 };
+
+// テンプレート関数
+/* public関数 */
+
+/*
+	単一の値を取得する関数
+	引数1: const std::string& key キー名
+	引数2: const size_t index 値のインデックス
+	返り値: T 値
+	備考: 値を文字列として返す場合は別途処理を定義しているため，この処理は数値を返すことを想定している
+*/
+template <typename T>
+T CCsvSettingFileReader::get_single_value(const std::string& key, const size_t index)
+{	
+	// std::string型のデータを返すget_single_valueは自身を呼び出さない別処理なので，再帰呼び出しにはならない(以下同様)
+	std::string value_str = get_single_value<std::string>(key, index);
+	/* 
+		整数型と浮動小数点型どちらを返すかによって文字列 -> 数値に変換する関数が異なる
+		それぞれの型について関数を定義することを防ぐため，一旦浮動小数点に変換し，そのあと必要な型にキャストするようにする
+		以下に定義する関数も同様
+	*/
+	T value = static_cast<T>(stof(value_str));
+
+	return value;
+}
+
+/*
+	すべての値を取得する関数
+	引数1: const std::string& key キー名
+	返り値: std::vector<T> 値
+*/
+template <typename T>
+std::vector<T> CCsvSettingFileReader::get_all_value(const std::string& key)
+{
+	std::vector<std::string> value_str = read_all_value(key);
+	std::vector<T> ret_value;
+
+	for (auto& v : value_str) {
+		ret_value.push_back(static_cast<T>(stof(v)));
+	}
+
+	return ret_value;
+}
+
+/*
+	指定した範囲のインデックスを持つ値を取得する関数
+	引数1: const std::string& key キー名
+	引数2: const size_t begin_index 範囲の開始インデックス
+	引数3: const size_t end_index 範囲の終了インデックス
+	返り値: std::vector<T> 値
+*/
+template <typename T>
+std::vector<T> CCsvSettingFileReader::get_ranged_value(const std::string& key, const size_t begin_index, const size_t end_index)
+{
+	std::vector<std::string> value_str = get_ranged_value<std::string>(key, begin_index, end_index);
+	std::vector<T> ret_value;
+
+	for (auto& v : value_str) {
+		ret_value.push_back(static_cast<T>(stof(v)));
+	}
+
+	return ret_value;
+}
+
+/*
+	単一の値を取得する関数(返す値の型が文字列の場合の処理)
+	引数1: const std::string& key キー名
+	引数2: const size_t index 値のインデックス
+	返り値: std::string 値
+	例外: インデックスが範囲外の場合
+*/
+template<>
+std::string CCsvSettingFileReader::get_single_value<std::string>(const std::string& key, const size_t index)
+{
+	token_array_t value = read_all_value(key);
+	if (index < value.size()) {
+		return value[index];
+	}
+
+	throw CHeisClientException("インデックスが範囲外です(インデックスの上限: %d, 指定されたインデックス: %d)", value.size() - 1, index);
+}
+
+/*
+	すべての値を取得する関数(返す値の型が文字列の場合の処理)
+	引数1: const std::string& key キー名
+	返り値: std::vector<std::string> 値
+*/
+template<>
+std::vector<std::string> CCsvSettingFileReader::get_all_value(const std::string& key)
+{
+	return read_all_value(key);
+}
+
+/*
+	指定した範囲のインデックスを持つ値を取得する関数(返す値の型が文字列の場合の処理)
+	引数1: const std::string& key キー名
+	引数2: const size_t begin_index 範囲の開始インデックス
+	引数3: const size_t end_index 範囲の終了インデックス
+	返り値: std::vector<std::string> 値
+	例外1: 開始インデックス > 終了インデックスの場合
+	例外2: インデックスが範囲外の場合
+*/
+template<>
+std::vector<std::string> CCsvSettingFileReader::get_ranged_value(const std::string& key, const size_t begin_index, const size_t end_index)
+{
+	std::vector<std::string> all_value = read_all_value(key);
+	std::vector<std::string> ret_value;
+
+	if (begin_index > end_index) {
+		throw CHeisClientException("開始インデックスが終了インデックスよりも後ろにあります(開始: %d, 終了: %d)", begin_index, end_index);
+	}
+	if (begin_index >= all_value.size()) {
+		throw CHeisClientException("インデックスが範囲外です(値の個数: %d, 開始: %d, 終了: %d)", all_value.size(), begin_index, end_index);
+	}
+
+	for (size_t i = begin_index; i <= end_index; i++) {
+		if (i == all_value.size()) {
+			break;
+		}
+		ret_value.push_back(all_value[i]);
+	}
+
+	return ret_value;
+}
