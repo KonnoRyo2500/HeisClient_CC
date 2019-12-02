aa5624082721f44aef99be9dee36e12e8b8657a8
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2019-12-02 22:28:09 +0900

    ・設定ファイルキー定義用のヘッダファイルを追加 ・オンラインモードで必要となるキーをひとまず定義した

---
+++ HeisClient_CC/src/setting_keys.h
@@ -0,0 +1,14 @@
+﻿// heis 設定ファイルキー一覧
+// Author: Ryo Konno
+#pragma once
+
+/* オンラインモード用 */
+// サーバのIPアドレス
+#define ONLINE_SETTING_KEY_SVR_ADDR "server_ip_addr"
+// サーバのポート番号
+#define ONLINE_SETTING_KEY_SVR_PORT "server_port_num"
+// 名前要求で，サーバに伝えるチーム名
+#define ONLINE_SETTING_KEY_TEAM_NAME "team_name"
+
+/* ローカルモード用 */
+// T.B.D.
