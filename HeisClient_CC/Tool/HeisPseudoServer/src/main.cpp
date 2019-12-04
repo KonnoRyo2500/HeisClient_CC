f7d014adb8b2dd61ee1076b425a869212c9b0e29
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2019-12-04 22:29:27 +0900

    ・設定ファイル対応 ・heis疑似サーバの待ち受けIPアドレス，ポート番号を設定ファイルから取得できるようにした

--- HeisClient_CC/Tool/HeisPseudoServer/src/main.cpp
+++ HeisClient_CC/Tool/HeisPseudoServer/src/main.cpp
@@ -5,8 +5,10 @@
 #include "JSON_sender.h"
 #include "JSON_receiver.h"
 #include "server_socket.h"
+#include "csv_setting_file_reader.h"
+#include "ps_setting_keys.h"
 
-#define IP_SELF "127.0.0.1"
+#define SETTING_FILE_NAME "heis_ps_setting.csv"
 
 /* グローバル変数 */
 // CCとの通信用ソケット
@@ -22,11 +24,13 @@ int main(int argc, char **argv)
 		CScenarioReader::ActionType act_type;
 		CJsonSender json_sender;
 		CJsonReceiver json_reader;
+		CCsvSettingFileReader setting_reader(SETTING_FILE_NAME);
 		
 		// ソケットの初期化
 		// クライアントは1人しか接続してこない想定
 		// TODO: ポート番号を設定ファイルから取得できるようにする
-		g_sck.sck_bind(20000, IP_SELF);
+		g_sck.sck_bind(setting_reader.get_single_value<uint16_t>(PS_SETTING_FILE_KEY_LISTEN_PORT, 0),
+					   setting_reader.get_single_value<std::string>(PS_SETTING_FILE_KEY_LISTEN_ADDR, 0));
 		g_sck.sck_listen();
 		g_sck.sck_accept();
 		
---
+++ HeisClient_CC/Tool/HeisPseudoServer/src/ps_setting_keys.h
@@ -0,0 +1,8 @@
+﻿// heisクライアント用疑似サーバ 設定ファイルキー一覧
+// Author: Ryo Konno
+#pragma once
+
+// CCからの接続を待ち受けるIPアドレス
+#define PS_SETTING_FILE_KEY_LISTEN_PORT "ps_listen_port_num"
+// CCからの接続を待ち受けるポート番号
+#define PS_SETTING_FILE_KEY_LISTEN_ADDR "ps_listen_ip_addr"
