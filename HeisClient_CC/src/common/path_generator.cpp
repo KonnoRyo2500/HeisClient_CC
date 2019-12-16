8e9677cbd866bac8d8f099a8131502244c22fd3b
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2019-12-16 22:37:10 +0900

    ・特定のパスを生成するためのクラスを新規作成

---
+++ HeisClient_CC/src/common/path_generator.cpp
@@ -0,0 +1,42 @@
+﻿// heis	パス生成クラス
+// Author: Ryo Konno
+
+#include "path_generator.h"
+#include "heis_client_exception.h"
+
+#include <climits>
+#ifdef WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+/* public関数 */
+
+/*
+	実行ファイルのパスを返す関数
+	引数なし
+	返り値: std::string 実行ファイルのパス 
+*/
+std::string CPathGenerator::get_exe_path()
+{
+#ifdef WIN32
+	char path_buf[MAX_PATH] = { 0 };
+
+	GetModuleFileNameA(NULL, path_buf, sizeof(path_buf));
+	std::string path = std::string(path_buf).substr(0, std::string(path_buf).find_last_of("\\"));
+	return path + "\\";
+#else
+	char path_buf[PATH_MAX] = { 0 };
+
+	if (readlink("/proc/self/exe", path_buf, sizeof(path_buf) - 1) > 0) {
+		std::string path = std::string(path_buf).substr(0, std::string(path_buf).find_last_of("/"));
+		return path + "/";
+	}
+	else {
+		throw CHeisClientException("シナリオファイルのパス取得に失敗しました");
+	}
+#endif
+}
+
+/* private関数 */
---
+++ HeisClient_CC/src/common/path_generator.h
@@ -0,0 +1,22 @@
+﻿// heis	パス生成クラス
+// Author: Ryo Konno
+#pragma once
+
+#include <string>
+
+class CPathGenerator {
+	// 構造体，列挙体など
+	public:
+
+	// メンバ関数
+	public:
+		// 実行ファイルのパスを取得
+		std::string get_exe_path();
+
+	private:
+
+
+	// メンバ変数
+	private:
+		// なし
+};
