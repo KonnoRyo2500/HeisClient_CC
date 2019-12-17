01238b976e0ae4a5e89e7986451315eb9170d527
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2019-12-17 22:19:10 +0900

    ・ローカルモードの設定を保持する構造体を定義

--- HeisClient_CC/src/local_mode/game_local.h
+++ HeisClient_CC/src/local_mode/game_local.h
@@ -11,6 +11,25 @@
 
 class CGameLocal {
 	// 構造体，列挙体など
+	public:
+		// ローカルモードの設定(設定ファイルから取得する)
+		struct LocalSetting {
+			// 自チーム名
+			std::string my_team_name;
+			// 敵チーム名
+			std::string enemy_team_name;
+			// フィールドの横幅
+			uint16_t field_width;
+			// フィールドの高さ
+			uint16_t field_height;
+			// 自チーム兵士の初期位置
+			std::vector<FieldPosition> my_team_init_pos;
+			// 敵チーム兵士の初期位置
+			std::vector<FieldPosition> enemy_team_init_pos;
+			// 自分のチームが先攻か
+			bool is_my_team_first;
+		};
+
 	private:
 		// 現在のターンに関する情報
 		enum CurrentTurn {
@@ -63,5 +82,7 @@ class CGameLocal {
 
 		// 疑似サーバ
 		CPseudoServer* m_pseudo_server;
-		
+
+		// 設定
+		LocalSetting m_setting;
 };
