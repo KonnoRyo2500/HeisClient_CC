07f704c80c1f34013434196de3ebae1b88d59cf8
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2020-05-14 19:52:12 +0900

    フォーマット文字列を展開する関数を作成

--- HeisClient_CC/src/common/common.h
+++ HeisClient_CC/src/common/common.h
@@ -5,3 +5,26 @@
 *	@details	CC内で共通して使用する処理を定義する．
 */
 #pragma once
+
+#include <string>
+#include <vector>
+
+namespace cc_common {
+
+	/* テンプレート関数 */
+	/**
+	*	@brief フォーマット文字列を展開する関数
+	*/
+	template <typename ... Args>
+	std::string format(const std::string& fmt, Args ... args)
+	{
+		// snprintf関数の返り値は、第一、第二引数に依らずfmtを展開した後の文字数と同等になる(NULL文字の分は含まれない)
+		// したがって、このようにsnprintfを呼ぶことで展開後の文字列の長さが得られる
+		size_t len = std::snprintf(NULL, 0, fmt.c_str(), args ...) + 1;
+		std::vector<char> fmt_expand(len);
+
+		// vectorはメモリの連続性が保証されている
+		std::snprintf(&fmt_expand[0], len, fmt.c_str(), args ...);
+		return std::string(&fmt_expand[0]);
+	}
+}
