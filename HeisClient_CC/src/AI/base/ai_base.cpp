6510c44aa30c38b3d61e2aef701ea28c69a6e17f
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2023-05-26 23:35:55 +0900

    AIのベースクラスを作成 ユーザは、このクラスを継承してAIを実装する仕様とする こうすることで、1つのクライアント内に複数のAIを実装でき、ユーザはその中から利用するAIを自由に選択できる

---
+++ HeisClient_CC/src/AI/base/ai_base.cpp
@@ -0,0 +1,18 @@
+/**
+*	@file		ai_base.cpp
+*	@brief		heis AIベースクラス
+*	@author		Ryo Konno
+*	@details	heis対戦AIのベースクラス。
+*/
+
+#include "ai_base.h"
+
+/**
+*	@brief コンストラクタ
+*	@param[in] commander 司令官インスタンス
+*/
+CAIBase::CAIBase(CCommander *commander)
+	: m_commander(commander)
+{
+	// Do Nothing
+}
---
+++ HeisClient_CC/src/AI/base/ai_base.h
@@ -0,0 +1,31 @@
+/**
+*	@file		ai_base.h
+*	@brief		heis AIベースクラス
+*	@author		Ryo Konno
+*	@details	heis対戦AIのベースクラス。
+*/
+
+#pragma once
+
+#include "commander.h"
+
+/**
+*	@brief	AIベースクラス
+*	@detail	heis対戦AIのベースクラス。ユーザは、このクラスを継承してAIを実装する
+*	@remark このクラスはインスタンス化できない(抽象クラス)
+*/
+class CAIBase
+{
+// メンバ関数
+public:
+	// コンストラクタ
+	explicit CAIBase(CCommander* commander);
+
+	// メイン処理
+	virtual void AI_main(const JSONRecvPacket_Board& board_pkt) = 0;
+
+// メンバ変数
+protected:
+	//! 兵士を操作するための司令官
+	CCommander* m_commander;
+};
