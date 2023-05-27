97032e68ad9f0a23ed3a9bdb16ba572d39c0c06a
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2023-05-27 11:12:41 +0900

    サンプルAIを実装

---
+++ HeisClient_CC/src/AI/implementation/sample_ai.cpp
@@ -0,0 +1,116 @@
+/**
+*	@file		sample_ai.cpp
+*	@brief		heis サンプルAI
+*	@author		Ryo Konno
+*	@details	AIのサンプル実装。ランダムに行動を選択する。
+*/
+
+#include "sample_ai.h"
+
+#include <random>
+
+/* public */
+
+/**
+*	@brief コンストラクタ
+*	@param[in] commander 司令官インスタンス
+*/
+CSampleAI::CSampleAI(CCommander* commander)
+	: CAIBase(commander)
+{
+	// Do Nothing
+}
+
+/**
+*	@brief AIのメイン処理
+*	@param[in] board_pkt 「盤面」パケット
+*/
+void CSampleAI::AI_main(const JSONRecvPacket_Board& board_pkt)
+{
+	std::string my_team_name = board_pkt.turn_team;
+
+	while (m_commander->get_all_actable_infantry_ids(my_team_name).size() != 0) {
+		std::string infantry_id;
+
+		if (sample_decide_action() == Action_Move) {
+			infantry_id = sample_select_next_infantry(m_commander->get_all_movable_infantry_ids(my_team_name));
+			if (infantry_id.size() > 0) {
+				sample_random_move(infantry_id);
+			}
+		}
+		else {
+			infantry_id = sample_select_next_infantry(m_commander->get_all_attackable_infantry_ids(my_team_name));
+			if (infantry_id.size() > 0) {
+				sample_random_attack(infantry_id);
+			}
+		}
+	}
+}
+
+/* private */
+
+/**
+*	@brief 兵士をランダムに移動させる関数
+*	@param[in] infantry_id 行動対象の兵士のID
+*/
+void CSampleAI::sample_random_move(const std::string infantry_id)
+{
+	std::vector<BoardPosition> movable_pos = m_commander->find_movable_position(infantry_id);
+	std::random_device rnd_dev;
+
+	if (movable_pos.size() > 0) {
+		BoardPosition dst_pos = movable_pos.at(rnd_dev() % movable_pos.size());
+
+		m_commander->move(infantry_id, dst_pos);
+	}
+}
+
+/**
+*	@brief 兵士をランダムな方向に攻撃させる関数
+*	@param[in] infantry_id 行動対象の兵士のID
+*/
+void CSampleAI::sample_random_attack(const std::string infantry_id)
+{
+	std::vector<BoardPosition> attackable_pos = m_commander->find_attackable_position(infantry_id);
+	std::random_device rnd_dev;
+
+	if (attackable_pos.size() > 0) {
+		BoardPosition dst_pos = attackable_pos.at(rnd_dev() % attackable_pos.size());
+
+		m_commander->attack(infantry_id, dst_pos);
+	}
+}
+
+/**
+*	@brief 次に行動する兵士をランダムに選択する関数
+*	@param[in] infantry_ids 行動可能なすべての兵士のID
+*	@return std::string 次に行動する兵士のID
+*/
+std::string CSampleAI::sample_select_next_infantry(const std::vector<std::string>& infantry_ids) const
+{
+	if (infantry_ids.size() == 0) {
+		return "";
+	}
+
+	std::random_device rnd_dev;
+	int infantry_idx = rnd_dev() % infantry_ids.size();
+
+	return infantry_ids[infantry_idx];
+}
+
+/**
+*	@brief 兵士に対して命令する行動を決定する関数
+*	@return CSampleAI::Action 次の行動
+*/
+CSampleAI::Action CSampleAI::sample_decide_action() const
+{
+	std::random_device rnd_dev;
+	int action_kind = rnd_dev() % 2;
+
+	if (action_kind == 0) {
+		return Action_Move;
+	}
+	else {
+		return Action_Attack;
+	}
+}
---
+++ HeisClient_CC/src/AI/implementation/sample_ai.h
@@ -0,0 +1,48 @@
+/**
+*	@file		sample_ai.h
+*	@brief		heis サンプルAI
+*	@author		Ryo Konno
+*	@details	AIのサンプル実装。ランダムに行動を選択する。
+*/
+
+#pragma once
+
+#include "ai_base.h"
+
+/**
+*	@brief サンプルAIクラス
+*	@details サンプル実装のAI。ランダムに行動を選択します。AI実装の際の参考としてお使いください。
+*/
+class CSampleAI : CAIBase
+{
+	// 構造体・列挙体など
+private:
+	/**
+	*	@enum Action
+	*	行動の種類
+	*/
+	enum Action {
+		//! 移動
+		Action_Move,
+		//! 攻撃
+		Action_Attack,
+	};
+
+	// メンバ関数
+public:
+	// コンストラクタ
+	explicit CSampleAI(CCommander* commander);
+
+	// メイン処理
+	void AI_main(const JSONRecvPacket_Board& board_pkt);
+
+private:
+	// ランダムな方向に移動する
+	void sample_random_move(const std::string infantry_id);
+	// ランダムな方向に攻撃する
+	void sample_random_attack(const std::string infantry_id);
+	// 次に行動する兵士を選択する
+	std::string sample_select_next_infantry(const std::vector<std::string>& infantry_ids) const;
+	// 兵士がどの行動をするか選択する
+	Action sample_decide_action() const;
+};
